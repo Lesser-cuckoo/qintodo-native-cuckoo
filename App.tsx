21945cec8007979b967a31101b0e8d0e4e93efa7
katan4722 <uver7770422@gmail.com>
2022-03-19 00:14:45 +0900

    ➕ react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider, RadioButton } from "react-native-paper";
+import { RadioButtonComponent } from "./components/ui/radiobutton";
+// import { RadioButton } from "./components/ui/radiobutton";
+// import { RadioButton } from "./components/ui/radiobutton";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        {/* <Text>Open up App.tsx to start working on your app!</Text> */}
+        {/* <StatusBar style="auto" /> */}
+        <RadioButtonComponent color="today" />
+        <RadioButtonComponent color="tomorrow" />
+        <RadioButtonComponent color="other" />
+      </View>
+    </PaperProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
