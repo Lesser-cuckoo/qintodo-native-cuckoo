057e59d6756be7d45922714b602767b0e1531913
katan4722 <uver7770422@gmail.com>
2022-03-19 01:34:05 +0900

    🎨 TailwindCSSに変更

--- component/ui/Avater.tsx
+++ component/ui/Avater.tsx
@@ -1,25 +1,19 @@
 import React from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, View } from "react-native";
+
+import tw from "twrnc";
 
 type Props = {
   avater?: string;
 };
 
 export const Avater = (props: Props) => {
-  const styles = StyleSheet.create({
-    avatar: {
-      height: 50,
-      width: 50,
-      borderRadius: 30,
-      marginRight: 8,
-    },
-  });
   const { avater } = props;
   return (
     <View>
       <Image
-        source={avater ? { url: avater } : require("./assets/avater.svg")}
-        style={styles.avatar}
+        source={avater ? { url: avater } : require("../../assets/avater.png")}
+        style={tw`h-12 w-12 rounded-full mr-2`}
       />
     </View>
   );
