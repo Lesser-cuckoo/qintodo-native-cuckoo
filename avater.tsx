87e53028c2be19854df17fa48e74b6ee59b03f16
katan4722 <uver7770422@gmail.com>
2022-03-19 01:22:14 +0900

    ✨ アバターコンポーネントの作成（UI) Fixes #10

---
+++ avater.tsx
@@ -0,0 +1,25 @@
+import { Image, StyleSheet, View } from "react-native";
+
+type Props = {
+  avater?: string;
+};
+
+export const Avater = (props: Props) => {
+  const styles = StyleSheet.create({
+    avatar: {
+      height: 50,
+      width: 50,
+      borderRadius: 30,
+      marginRight: 8,
+    },
+  });
+  const { avater } = props;
+  return (
+    <View>
+      <Image
+        source={avater ? { url: avater } : require("./assets/avater.svg")}
+        style={styles.avatar}
+      />
+    </View>
+  );
+};
