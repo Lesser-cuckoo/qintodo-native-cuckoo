c917b1149201d55bc42385e599a8ef95e28df8a1
katan4722 <uver7770422@gmail.com>
2022-04-16 03:00:01 +0900

    💄 ヘッダーの追加

--- src/screens/ThemeScreen.tsx
+++ src/screens/ThemeScreen.tsx
@@ -2,6 +2,7 @@ import React, { VFC } from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import tw from "twrnc";
 import Icon from "react-native-vector-icons/AntDesign";
+import { MyPageHeader } from "src/components/MyPageHeader";
 
 export const bgColor = {
   red: "#EF4444",
@@ -12,34 +13,43 @@ export const bgColor = {
   other: "#FBBF24",
 } as const;
 
-const ThemeScreen: VFC = () => {
+type Props = {
+  navigation: any;
+};
+
+const ThemeScreen = (props: Props) => {
+  const { navigation } = props;
+
   const handleClick = () => {
     alert("clicked");
   };
   return (
-    <View style={tw`flex-1 pt-6`}>
-      <TouchableOpacity
-        style={tw`py-2 px-5 flex flex-row justify-between`}
-        onPress={handleClick}
-      >
-        <Text style={tw`font-bold text-base`}>端末の設定に合わせる</Text>
-        <Icon name="check" color="blue" size={24} />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={tw`py-2 px-5 flex flex-row justify-between`}
-        onPress={handleClick}
-      >
-        <Text style={tw`font-bold text-base`}>ライト</Text>
-        <Icon name="check" color="blue" size={24} />
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={tw`py-2 px-5 flex flex-row justify-between`}
-        onPress={handleClick}
-      >
-        <Text style={tw`font-bold text-base`}>ダーク</Text>
-        <Icon name="check" color="blue" size={24} />
-      </TouchableOpacity>
-    </View>
+    <>
+      <MyPageHeader title="テーマ" navigation={navigation} />
+      <View style={tw`flex-1 pt-6`}>
+        <TouchableOpacity
+          style={tw`py-2 px-5 flex flex-row justify-between`}
+          onPress={handleClick}
+        >
+          <Text style={tw`font-bold text-base`}>端末の設定に合わせる</Text>
+          <Icon name="check" color="blue" size={24} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw`py-2 px-5 flex flex-row justify-between`}
+          onPress={handleClick}
+        >
+          <Text style={tw`font-bold text-base`}>ライト</Text>
+          <Icon name="check" color="blue" size={24} />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw`py-2 px-5 flex flex-row justify-between`}
+          onPress={handleClick}
+        >
+          <Text style={tw`font-bold text-base`}>ダーク</Text>
+          <Icon name="check" color="blue" size={24} />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 };
 
