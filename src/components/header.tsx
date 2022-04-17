7c919a2e468f5fba01abcc5e87e0f037f892dbd3
katan4722 <uver7770422@gmail.com>
2022-04-17 20:52:51 +0900

    ✨ アバターを表示させるようにした

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,14 @@
 import { TouchableOpacity, View, Image } from "react-native";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import tw from "twrnc";
 import { QinTodoIcon } from "src/components/ui/QinTodoIcon";
 import { Avater } from "src/components/ui/Avater";
-// import {
-//   addNewProfile,
-//   // getAvatarUrl,
-//   getProfile,
-// } from "src/libs/SupabaseClient";
-// import { useUser } from "src/components/UserContext";
+import {
+  addNewProfile,
+  // getAvatarUrl,
+  getProfile,
+} from "src/libs/SupabaseClient";
+import { useUser } from "src/components/UserContext";
 
 type Props = {
   navigation: any;
@@ -20,42 +20,42 @@ export const Header = (props: Props) => {
     navigation.navigate("MyPage");
   };
 
-  // const { user } = useUser();
-  // // const { errorToast } = useToast();
-  // const [avatar, setAvatar] = useState<string>("");
+  const { user } = useUser();
+  // const { errorToast } = useToast();
+  const [avatar, setAvatar] = useState<string>("");
 
-  // const fetchProfile = useCallback(
-  //   async (uid: string) => {
-  //     if (user) {
-  //       const profile = await getProfile(user.id);
-  //       console.log(profile);
+  const fetchProfile = useCallback(
+    async (uid: string) => {
+      if (user) {
+        const profile = await getProfile(user.id);
+        console.log(profile);
 
-  //       if (profile) {
-  //         setAvatar(profile.avatar);
-  //       } else {
-  //         const username = user.user_metadata.name
-  //           ? user.user_metadata.name
-  //           : "ユーザー";
-  //         const avatar_url = user.user_metadata.avatar_url
-  //           ? user.user_metadata.avatar_url
-  //           : "";
-  //         const isOk = await addNewProfile(uid, username, avatar_url);
-  //         if (!isOk) {
-  //           alert("プロフィールの新規登録に失敗しました。");
-  //         } else {
-  //           setAvatar(avatar_url);
-  //         }
-  //       }
-  //     }
-  //   },
-  //   [user]
-  // );
+        if (profile) {
+          setAvatar(profile.avatar);
+        } else {
+          const username = user.user_metadata.name
+            ? user.user_metadata.name
+            : "ユーザー";
+          const avatar_url = user.user_metadata.avatar_url
+            ? user.user_metadata.avatar_url
+            : "";
+          const isOk = await addNewProfile(uid, username, avatar_url);
+          if (!isOk) {
+            alert("プロフィールの新規登録に失敗しました。");
+          } else {
+            setAvatar(avatar_url);
+          }
+        }
+      }
+    },
+    [user]
+  );
 
-  // useEffect(() => {
-  //   if (user) {
-  //     fetchProfile(user.id);
-  //   }
-  // }, [user, fetchProfile]);
+  useEffect(() => {
+    if (user) {
+      fetchProfile(user.id);
+    }
+  }, [user, fetchProfile]);
 
   return (
     <View style={tw`relative h-22 w-full bg-white`}>
@@ -67,7 +67,7 @@ export const Header = (props: Props) => {
           style={tw`right-6 absolute rounded-full overflow-hidden bg-indigo-300`}
         >
           <TouchableOpacity onPress={handleOpenMyPage}>
-            {/* {avatar ? (
+            {avatar ? (
               <View>
                 <Image
                   source={{ uri: `${avatar}` }}
@@ -77,9 +77,9 @@ export const Header = (props: Props) => {
                   }}
                 />
               </View>
-            ) : ( */}
-            <Avater rounded size={10} />
-            {/* )} */}
+            ) : (
+              <Avater rounded size={10} />
+            )}
           </TouchableOpacity>
         </View>
       </View>
